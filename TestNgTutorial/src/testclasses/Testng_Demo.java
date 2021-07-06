2913f8a98b4883e4015d73b3988d49db848f8eea
Debopam <debopamroytata@outlook.com>
2021-07-06 10:45:58 +0530

    First commit

---
+++ TestNgTutorial/src/testclasses/Testng_Demo.java
@@ -0,0 +1,15 @@
+package testclasses;
+
+import org.testng.annotations.Test;
+
+public class Testng_Demo {
+  @Test
+  public void testMethod1() {
+	  System.out.println("This is the test method 1");
+  }
+  
+  @Test
+  public void testMethod2() {
+	  System.out.println("This is the test method 2");
+  }
+}
